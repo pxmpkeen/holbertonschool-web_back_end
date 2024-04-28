003fd96f68b8bb9917aad206474ac9808b85a04b
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 00:41:05 +0400

    3-to_str.py

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating variables and function with float and str
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function that str's num
+    """
+    return str(n)
