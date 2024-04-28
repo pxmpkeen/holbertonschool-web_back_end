82d72c76398b8ecab04cc07e0ba3f5e600d8d994
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 00:36:39 +0400

    1-concat.py

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating variables and function with str
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Function concatenates strings
+    """
+    return "{}{}".format(str1, str2)
