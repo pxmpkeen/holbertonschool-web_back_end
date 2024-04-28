bc0080e3d7f89dd99d9e36f79761ce5ee7566fd1
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 00:39:19 +0400

    2-floor.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Annotating variables and function with float and int
+"""
+import math
+
+
+def floor(n: float) -> int:
+    """
+    Function that floors num
+    """
+    return math.floor(n)
