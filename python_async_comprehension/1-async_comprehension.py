69e440e92df38515500a749d523d72746a4dc3b5
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 03:46:06 +0400

    1-async_comprehension.py

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,15 +1,14 @@
 #!/usr/bin/env python3
 """
-Async Generator
+Async Comprehension
 """
 import asyncio
-import random
-from typing import Generator
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
 
-async def async_generator() -> Generator[float, None, None]:
+
+async def async_comprehension() -> List[float]:
     """
-    async generator
+    async comprehension
     """
-    for i in range(10):
-        await asyncio.sleep(10)
-        yield random.uniform(0, 10)
+    return [i async for i in async_generator()]
