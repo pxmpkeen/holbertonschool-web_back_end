f3d351a563c8b3cf2fd8e59d718c22013ee9ec70
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 01:45:58 +0400

    0-basic_async_syntax.py

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Async: beginning
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    async func
+    """
+    val = random.randrange(0, max_delay)
+    await asyncio.sleep(val)
+    return val
