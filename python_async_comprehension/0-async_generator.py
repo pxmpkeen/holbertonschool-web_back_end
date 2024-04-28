fc2b4eb56f0384b387ab7e2c8286a03e666a5355
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 03:37:31 +0400

    test commit

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -11,5 +11,5 @@ async def async_generator() -> Generator[float, None, None]:
     async generator
     """
     for i in range(10):
-        await asyncio.sleep(10)
+        await asyncio.sleep(1)
         yield random.uniform(0, 10)
