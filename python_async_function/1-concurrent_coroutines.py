900be89fb7beb1c2a663bea0c5bfcfde2bf8495c
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 02:13:31 +0400

    0-basic_async_syntax.py 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -11,6 +11,7 @@ async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     async func v2
     """
-    delays: List[float] = list()
-    [delays.append(await wait_random(max_delay)) for mirt in range(n)]
+    delays: List[float] = []
+    for i in range(n):
+        delays.append(await wait_random(max_delay))
     return sorted(delays)
