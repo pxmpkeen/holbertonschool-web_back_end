12f1eb51cf6794924bac62370928e8b02d901acd
pxmpkeen <pxmpkeen@gmail.com>
2024-04-29 00:48:57 +0400

    5-sum_list.py

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+Annotating variables and function with float and str
+"""
+
+
+def sum_list(input_list: List[float]) -> float:
+    """
+    Function that str's num
+    """
+    return sum(input_list)
