46a77daac5baf226993d1015c37f23794ad36d85
pxmpkeen <pxmpkeen@gmail.com>
2024-05-01 18:31:02 +0400

    100-safe_first_element.py

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -2,11 +2,11 @@
 """
 Annotating variables and function with complex types
 """
-from typing import Any, Optional, Sequence
+from typing import Any, Union, Sequence
 
 
 # The types of the elements of the input are not known
-def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
     if lst:
         return lst[0]
     else:
