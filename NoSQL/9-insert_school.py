d8feb3454911275473c34674419b9e039ed2100f
pxmpkeen <pxmpkeen@gmail.com>
2024-05-06 00:58:57 +0400

    9-insert_school.py (Deprecated method)

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -11,4 +11,4 @@ def insert_school(mongo_collection, **kwargs):
     """
     if len(kwargs) == 0:
         return None
-    return mongo_collection.insert_one(kwargs)
+    return mongo_collection.insert(kwargs)
