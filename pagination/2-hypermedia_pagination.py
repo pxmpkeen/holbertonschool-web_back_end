50524338de2fd185c053fb8815f4bb1d44ad0ef8
pxmpkeen <pxmpkeen@gmail.com>
2024-05-09 16:41:07 +0400

    hot fix

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -53,7 +53,7 @@ class Server:
             "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": page + 1 if page < total_len else None,
+            "next_page": page + 1 if page < total_pages else None,
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages
             }
