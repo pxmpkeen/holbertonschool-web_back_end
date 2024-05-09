3916192c77b3de32266da6e0675bf16c99024dfd
pxmpkeen <pxmpkeen@gmail.com>
2024-05-09 16:34:51 +0400

    hot fix

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -45,13 +45,14 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """Returning dictionary with info about page
         """
+        data = self.get_page(page=page, page_size=page_size)
         total_len = len(self.dataset())
-        total_pages = total_len // page_size
+        total_pages = math.ceil(total_len / page_size)
 
         return {
-            "page_size": page_size,
+            "page_size": len(data),
             "page": page,
-            "data": self.get_page(page=page, page_size=page_size),
+            "data": data,
             "next_page": page + 1 if page < total_len else None,
             "prev_page": page - 1 if page > 1 else None,
             "total_pages": total_pages
