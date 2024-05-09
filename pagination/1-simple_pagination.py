d1e6d8294fa3a99aa4e0e9a71798ec8e6cf7dcfe
pxmpkeen <pxmpkeen@gmail.com>
2024-05-09 16:11:17 +0400

    pep8 hotfix

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -29,9 +29,11 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Getting page
+        """
         assert type(page) is int and page > 0
         assert type(page_size) is int and page_size > 0
-        
+
         indexes = index_range(page, page_size)
         s, e = indexes[0], indexes[1]
 
