70283c14f4f36f0a49c16f1e5f2bdc5f7a6667b1
pxmpkeen <pxmpkeen@gmail.com>
2024-05-13 14:46:13 +0400

    4-pricing.js

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -36,6 +36,13 @@ export default class Pricing {
   }
 
   static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('conversionRate must be a number');
+    }
     return amount * conversionRate;
   }
 }
