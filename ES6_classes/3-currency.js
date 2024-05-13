98fecc32b4e11838da6f0389c0714093e3329d8d
pxmpkeen <pxmpkeen@gmail.com>
2024-05-13 14:44:06 +0400

    3-currency.js

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -2,6 +2,14 @@ export default class Currency {
   constructor(code, name) {
     this._code = code;
     this._name = name;
+
+    if (typeof this._code !== 'string') {
+      throw new TypeError('code must be a string');
+    }
+
+    if (typeof this._name !== 'string') {
+      throw new TypeError('name must be a string');
+    }
   }
 
   get code() {
